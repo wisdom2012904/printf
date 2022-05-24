047c91d996317a600130faa555ce21fb58521cb4
aoko-code <cynthiakhamenge@yahoo.com>
2022-05-23 18:07:03 -0700

    comment print.c

--- print.c
+++ print.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
+ */
 int _printf(const char *format, ...)
 {
 	int (*pfunc)(va_list, flags_t *);
